f5f2c2be2ebd9d95e538b1111188de3721ac6c2b
pyrtaAV <pyrta90@gmail.com>
2023-06-19 02:10:27 +0400

    Добавлены метод splitInput для разделения входных данных и метод resultCalc для выполнения основных арифметических операций

--- Main.java
+++ Main.java
@@ -1,9 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calc("Hello"));
+        Scanner sc = new Scanner(System.in);
+        String testText = sc.nextLine();
+        System.out.println(calc(testText));
     }
 
     public static String calc(String input) {
-        return input;
+        String[] inputs = splitInput(input);
+        int result = resultCalc(inputs);
+        return String.valueOf(result);
+    }
+
+    public static String[] splitInput(String input) {
+        return input.split(" ");
+    }
+
+    public static int resultCalc(String[] input) {
+        switch (input[1]) {
+            case "+":
+                return Integer.parseInt(input[0]) + Integer.parseInt(input[2]);
+            case "-":
+                return Integer.parseInt(input[0]) - Integer.parseInt(input[2]);
+            case "/":
+                return Integer.parseInt(input[0]) / Integer.parseInt(input[2]);
+            case "*":
+                return Integer.parseInt(input[0]) * Integer.parseInt(input[2]);
+        }
+        return 0;
     }
 }
